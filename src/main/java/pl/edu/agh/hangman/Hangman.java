a2ea59e4de8d8f89d658138cc13a1026b2b5a53b
Dorota Puto <dootras@gmail.com>
2021-01-23 11:24:24 +0100

    Game status implemented

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -62,10 +62,14 @@ public class Hangman {
     public static void main(String[] args) {
     	PuzzleFactory pz = new PuzzleFactory();
     	GuessChecker guessChecker = new GuessChecker();
+    	GameStatus status = new GameStatus();
         	
     	System.out.println(pz.getPuzzle());
     	
     	guessChecker.checkGuess("kot", Arrays.asList("o"));
+    	status.printStatus("kot", Arrays.asList("o", "s"));
+    	
+    	System.out.println("Lets start!");
     	
     	
     	
